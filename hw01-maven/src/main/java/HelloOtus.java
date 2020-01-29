3548df11e6fcd201f254c6f03ecceccab43eda28
ilyafilim <ilya.filim@gmail.com>
2020-01-29 19:28:21 +0300

    hw-1

---
+++ hw01-maven/src/main/java/HelloOtus.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        System.out.println(new IntegerInBox(1).getI());
+        System.out.println(new IntegerInBox(new Random().nextInt()).getI());
+        try {
+            System.out.println(new IntegerInBox(null).getI());
+        } catch (NullPointerException npe) {
+            System.out.println("null has been caught before method getI()");
+        }
+
+    }
+}
+
+class IntegerInBox {
+    private final Integer i;
+
+    public IntegerInBox(Integer i) {
+        this.i = checkNotNull(i);
+    }
+
+    public int getI() {
+        return i;
+    }
+}
